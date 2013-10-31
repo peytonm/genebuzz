70b5193641b76a460510d2a52fd70c92e62c6495
peyton <peyton@peytonmccullough.com>
2013-10-31 18:32:33 -0400

    Add description.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject genebuzz "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A tongue-in-cheek implementation of FizzBuzz using a genetic algorithm."
   :dependencies [[org.clojure/clojure "1.5.1"]
                                [org.clojure/tools.cli "0.2.4"]]
   :main genebuzz.core)
